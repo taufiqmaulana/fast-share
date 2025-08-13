96ec8c08fe5180c1f8b892d1d0de7a665c5f1161
Taufiq Maulana <fixy.maulana@gmail.com>
2025-08-13 19:55:27 +0700

    complete linux installation script

--- install-linux.sh
+++ install-linux.sh
@@ -4,7 +4,19 @@ workdir="/home/$USER/.local/fast-share.app"
 mkdir -p "$workdir"
 cd "$workdir"
 
-cat > /home/$USER/.local/fast-share.app/fast-share.desktop <<EOL
+echo "Cleaning up previous installation ..."
+rm -f /home/$USER/.local/bin/fshare
+rm -f /home/$USER/.local/share/applications/fast-share.desktop
+rm -f fhsare
+rm -f icon.png
+
+echo "Downloading binary ..."
+curl -L -o fshare https://github.com/taufiqmaulana/fast-share/releases/download/v1.0.0-beta/fshare-linux-amd64
+curl -L -o icon.png https://raw.githubusercontent.com/taufiqmaulana/fast-share/refs/heads/master/icon.png
+
+ln -s /home/$USER/.local/fast-share.app/fshare /home/$USER/.local/bin/fshare
+
+cat > /home/$USER/.local/share/applications/fast-share.desktop <<EOL
 [Desktop Entry]
 Type=Application
 Name=Fast Share
@@ -13,3 +25,7 @@ NoDisplay=true
 Icon=/home/$USER/.local/fast-share.app/icon.png
 Exec=/home/$USER/.local/fast-share.app/fshare "%f"
 EOL
+
+chmod +x /home/$USER/.local/fast-share.app/fshare
+update-desktop-database /home/$USER/.local/share/applications
+echo "Installation completed"
